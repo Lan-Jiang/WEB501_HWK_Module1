15e025fadd80b8f15178446b533c5dbb43ace640
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-01-24 23:51:47 -0800

    WEB501 HWK Wk4 Day1 splitted js to external files

---
+++ js/digclock.js
@@ -0,0 +1,20 @@
+/* create clock and show time zone */
+var photoList = ["bustour", "cabinrental", "campingadv", "collegetours", "rentalbike", "tourgroup"];
+var photos = [];
+var fileNames = [];
+var imageList = [];
+var image;
+var openList = "<li class='partner'>";
+var closeList = "</li>";
+
+// images/partners/partner-bustour.png
+//Create a loop to create 6 images
+for (var i=0; i<6; i++) {
+    fileNames.push("partner-" + photoList[i]);
+    photos.push("<img src = 'images/partners/" +fileNames[i]+ ".png' >");//  
+    image = openList + photos[i] + closeList;
+    imageList.push(image);
+
+    //Display 6 images from array
+    document.getElementById("partners").innerHTML = imageList.join(" ");
+}
