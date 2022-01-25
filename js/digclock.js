dce074235a41284c5a4507a303f8e977a246eafa
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-01-25 00:22:11 -0800

    WEB501 HWK Wk4 Day1 added generatecode js not finished

--- js/digclock.js
+++ js/digclock.js
@@ -1,20 +1,75 @@
-/* create clock and show time zone */
-var photoList = ["bustour", "cabinrental", "campingadv", "collegetours", "rentalbike", "tourgroup"];
-var photos = [];
-var fileNames = [];
-var imageList = [];
-var image;
-var openList = "<li class='partner'>";
-var closeList = "</li>";
+//Create a time data function
+function currentTime() {
+    //Declare variables
+    var d = new Date(); //Get current date
+    var hr = d.getHours(); //Get current hours
+    var min = d.getMinutes(); //Get current minutes
+    var sec = d.getSeconds(); //Get current seconds
+    var ampm; //Declare empty variable to store AM or PM
 
-// images/partners/partner-bustour.png
-//Create a loop to create 6 images
-for (var i=0; i<6; i++) {
-    fileNames.push("partner-" + photoList[i]);
-    photos.push("<img src = 'images/partners/" +fileNames[i]+ ".png' >");//  
-    image = openList + photos[i] + closeList;
-    imageList.push(image);
+    //Add 0 to single digits for seconds
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    //Add 0 to single digits for minutes
+    if (min < 10) {
+        min = "0" + min;
+    }
 
-    //Display 6 images from array
-    document.getElementById("partners").innerHTML = imageList.join(" ");
+    //Determine AM or PM string
+    if (hr == 12) {
+        ampm = "PM"; //Set to PM
+    } else if (hr > 12) {
+        hr -= 12; //Deduct 12 from hours gerater than 12
+        ampm = "PM"; //Set to PM
+    } else {
+        ampm = "AM";
+    }
+
+    //Add time zone
+    var utchr = d.getUTCHours(); //Get current UTC 
+    var timeDiff = utchr - hr; //To store time difference between GMT and local hour
+    var adjTimeDiff; //To store time difference converted to positive number
+    var timeZone; //To store the 4 time zomes (PT, MT, CT, ET)
+
+    if (timeDiff < 0) {
+        adjTimeDiff = utchr + 24 - (hr + 12)
+    } 
+
+    //Determine time zone
+    if (timeDiff >= 0) {
+        if (timeDiff == 8) {
+            timeZone = 'PT';
+        } else if (timeDiff == 7) {
+            timeZone = 'MT';
+        } else if (timeDiff == 6) {
+            timeZone = 'CT';
+        } else if (timeDiff == 5) {
+            timeZone = 'ET';
+        }
+    } else {
+        if (adjTimeDiff  == 8) {
+            timeZone = 'PT';
+        } else if (adjTimeDiff  == 7) {
+            timeZone = 'MT';
+        } else if (adjTimeDiff  == 6) {
+            timeZone = 'CT';
+        } else if (adjTimeDiff  == 5) {
+            timeZone = 'ET';
+        } else {
+            timeZone = 'Unknown';
+        }
+    }
+
+    //Assemble time format to display
+    var time = hr + ":" + min + ":" + sec + " " + ampm + " " + timeZone;
+
+    //Display current local time and time zone on HTML elements
+    document.getElementById("clock").innerText = time; //Adding time
+
+    //Run time data function every 1 second
+    setInterval(currentTime, 1000); //Setting timer  
+
 }
+//Initial run of time data function
+currentTime();
