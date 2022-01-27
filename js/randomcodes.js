42737b03845e8e61b20e4ebd8768fbd2ede32335
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-01-26 23:33:58 -0800

    WEB501 HWK Wk4 Day2 added js of show/hide sumbit button unfinished

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -4,6 +4,8 @@
 function generateCode() {
     //Create variables to store generated codes and the type of characters we want to show as codes
     var code = ' ';//initialize to null value
+    var getCode = ' ';//to store entered code
+    var btnvalue; // for buttom boolean value
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 
     //Generate character multiple times using a loop
@@ -19,9 +21,28 @@ function generateCode() {
 document.getElementById('codes').innerHTML = generateCode();
 
 //Disable Button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue;
+    if (btnvalue == true) {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
+    var codebox = document.getElementById("codeentered");
+    codebox.addEventListener("input", evaluateCode);
 };
+
+function evaluationCode() {
+    getCode = document.getElementById("codeentered").value; 
+    var charset1 = getCode.trim();
+    var charset2 = code.trim();
+    //test if code entered matches the number of generated codes
+    if (charset1.length == charset2.length && charset1 == charset1) {
+        disableButton(false);
+    }
+}
 
 //Active function
 disableButton();
