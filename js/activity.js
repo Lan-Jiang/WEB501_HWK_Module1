e48897e6c09833456719673412cf91fa90e5c0ee
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-02-03 21:28:53 -0700

    WEB501 HWK Wk5 D1 added click and cursor are hands

--- js/activity.js
+++ js/activity.js
@@ -1,8 +1,22 @@
 //Add toggle click
 $(document).ready(function() {
-    $("table").find("td").click(function() {
+    //select 'not available'
+
+    //mouseover with cursor hand, exclude td first child
+    $("table").find("td").not(":nth-child(1)").mouseover(function() {
+        $(this).css('cursor', 'pointer');
+        
+    });
+
+    //click to change background
+    // $("table").find("td").not('#notavailable').on('click', function (){ 
+    //     $(this).toggleClass("selected");
+    // });
+    //:not is twice faster than .not()
+    $("table td:not('#notavailable')").on('click', function (){ 
         $(this).toggleClass("selected");
     });
+
 });
 
 
