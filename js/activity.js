b1d6792cd81a6ea3fe9fa4051839e81e84feb5b8
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2022-02-06 00:05:56 -0700

    WEB501 HWK Wk5 D3 add displaySelected 1/2

--- js/activity.js
+++ js/activity.js
@@ -1,11 +1,8 @@
 //Add toggle click
 $(document).ready(function() {
-    //select 'not available'
-
     //mouseover with cursor hand, exclude td first child
     $("table").find("td").not(":nth-child(1)").mouseover(function() {
-        $(this).css('cursor', 'pointer');
-        
+        $(this).css('cursor', 'pointer');       
     });
 
     //click to change background
@@ -13,11 +10,40 @@ $(document).ready(function() {
     //     $(this).toggleClass("selected");
     // });
     //:not is twice faster than .not()
-    $("table td:not('#notavailable')").on('click', function (){ 
-        $(this).toggleClass("selected");
+    // $("table td:not('#notavailable')").on('click', function (){ 
+    //     $(this).toggleClass("selected");
+    // });
+
+    //click function and show info box
+    $("td").click(function() {
+        var content = $(this).text();
+
+        if (content != "Not Available") {
+            $(this).toggleClass("selected");
+
+            if ($(this).hasClass("selected")) {
+                $("#displaySelected").css("visibility", "visible");
+                $("#displaySelected").css("margin-top", "2em");
+                $("#result").append("<p>" + content + "</p>");             
+            } else {
+                $("#result p:contains(" + content + ")").remove();
+
+                if ($("#result").has("p").length == false) {
+                    $("#displaySelected").css("visibility", "hidden");
+                    $("#displaySelected").css("margin-top", "0");
+                }
+            }
+        }
     });
 
 });
+
+//Hide displaySelected when load
+function hidedisplaySelected() {
+    document.getElementById("displaySelected").style.visibility = "hidden";
+};
+
+hidedisplaySelected();
 
 
 
